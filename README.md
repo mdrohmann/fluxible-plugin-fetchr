6b8e4b1a2bef0995ff5d72ce5877f54e8478350f
Michael Ridgway <mridgway@yahoo-inc.com>
2014-10-30 20:48:44 -0700

    Ready for open source

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-fluxible-plugin-fetchr
-======================
+# Fetchr Plugin for Fluxible App
+[![Build Status](https://travis-ci.org/yahoo/fluxible-plugin-fetchr.svg?branch=master)](https://travis-ci.org/yahoo/fluxible-plugin-fetchr) [![Dependency Status](https://david-dm.org/yahoo/fluxible-plugin-fetchr.svg)](https://david-dm.org/yahoo/fluxible-plugin-fetchr) [![Coverage Status](https://coveralls.io/repos/yahoo/fluxible-plugin-fetchr/badge.png?branch=master)](https://coveralls.io/r/yahoo/fluxible-plugin-fetchr?branch=master)
+
+Provides isomorphic RESTful service access to your [Fluxible application](github.com/yahoo/fluxible-app) using [fetchr](github.com/yahoo/fetchr).
+
+## Usage
+
+```js
+var FluxibleApp = require('fluxible-app');
+var fetchrPlugin = require('fluxible-plugin-fetchr');
+var app = new FluxApplication();
+
+app.plug(fetchrPlugin({
+    xhrPath: '/api' // Path for XHR to be served from
+}));
+```
+
+Now, when calling the `createContext` method on the server, make sure to send in the request object:
+
+```
+app.createContext({
+    req: req
+});
+```
+
+## Fluxible Methods Added
+
+### actionContext
+
+ * `actionContext.service.read(resource, params, [config,] callback)`: Call the read method of a service. See [https://github.com/yahoo/fetchr](fetchr docs) for more information.
+ * `actionContext.service.create(resource, params, body, [config,] callback)`: Call the create method of a service. See [https://github.com/yahoo/fetchr](fetchr docs) for more information.
+ * `actionContext.service.update(resource, params, body, [config,] callback)`: Call the update method of a service. See [https://github.com/yahoo/fetchr](fetchr docs) for more information.
+ * `actionContext.service.delete(resource, params, [config,] callback)`: Call the delete method of a service. See [https://github.com/yahoo/fetchr](fetchr docs) for more information.
+ * `actionContext.getServiceMeta()`: The plugin will collect metadata for service responses and provide access to it via this method. This will return an array of metadata objects.
+
+## Other Methods
+
+The plugin also provides access to some internals and the options that were passed in.
+
+```
+var pluginInstance = fetchrPlugin({
+    xhrPath: '/api'
+});
+
+pluginInstance.getXhrPath(); // returns '/api'
+pluginInstance.getServiceClass(); // returns the fetchr instance used by the plugin
+```
+
+## Registering Your Services
+
+Since the fetchr plugin is in control the fetchr class, we expose this through the `getServiceManager` method.
+
+```js
+pluginInstance.registerService(yourService);
+```
+
+Or if you need to do this from your application without direct access to the plugin
+
+```js
+app.getPlugin('FetchrPlugin').registerService(yourService);
+```
+
+## Exposing Your Services
+
+Fetchr also contains an express/connect middleware that can be used as your access point from the client.
+
+```js
+var server = express();
+server.use(pluginInstance.getXhrPath(), pluginInstance.getMiddleware());
+```
+
+## License
+
+This software is free to use under the Yahoo! Inc. BSD license.
+See the [LICENSE file][] for license text and copyright information.
+
+[LICENSE file]: https://github.com/yahoo/fluxible-plugin-fetchr/blob/master/LICENSE.md
---
+++ tests/unit/lib/fetchr-plugin.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright 2014, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+/*globals describe,it,beforeEach */
+"use strict";
+
+var path = require('path');
+var expect = require('chai').expect;
+var fetchrPlugin = require('../../../lib/fetchr-plugin');
+var FluxibleApp = require('fluxible-app');
+var mockService = require('../../fixtures/services/test');
+
+describe('fetchrPlugin', function () {
+    var app,
+        pluginInstance,
+        context,
+        mockReq;
+
+    beforeEach(function () {
+        mockReq = {};
+        app = new FluxibleApp();
+        pluginInstance = fetchrPlugin({
+            xhrPath: 'custom/api'
+        });
+        pluginInstance.registerService(mockService);
+        app.plug(pluginInstance);
+        context = app.createContext({
+            req: mockReq
+        });
+    });
+
+    describe('factory', function () {
+        it('should use default xhr path', function () {
+            var p = fetchrPlugin();
+            expect(p.getXhrPath()).to.equal('/api');
+        });
+    });
+
+    describe('actionContext', function () {
+        var actionContext;
+        beforeEach(function () {
+            actionContext = context.getActionContext();
+        });
+        describe('service', function () {
+            it('should have a service interface', function () {
+                expect(actionContext.service).to.be.an('object');
+                expect(actionContext.service.create).to.be.an('function');
+                expect(actionContext.service.read).to.be.an('function');
+                expect(actionContext.service.update).to.be.an('function');
+                expect(actionContext.service['delete']).to.be.an('function');
+            });
+            describe('create', function () {
+                it('should call the service\'s create method', function (done) {
+                    actionContext.service.read('test', {}, {}, function (err, result) {
+                        expect(result).to.equal('read');
+                        expect(actionContext.getServiceMeta()).to.deep.equal([
+                            {
+                                headers: {
+                                    'Cache-Control': 'private'
+                                }
+                            }
+                        ]);
+                        done();
+                    });
+                });
+            });
+            describe('read', function () {
+                it('should call the service\'s read method', function (done) {
+                    actionContext.service.create('test', {}, {}, function (err, result) {
+                        expect(result).to.equal('create');
+                        expect(actionContext.getServiceMeta()).to.be.empty;
+                        done();
+                    });
+                });
+            });
+            describe('update', function () {
+                it('should call the service\'s update method', function (done) {
+                    actionContext.service.update('test', {}, {}, function (err, result) {
+                        expect(result).to.equal('update');
+                        expect(actionContext.getServiceMeta()).to.be.empty;
+                        done();
+                    });
+                });
+            });
+            describe('delete', function () {
+                it('should call the service\'s delete method', function (done) {
+                    actionContext.service['delete']('test', {}, function (err, result) {
+                        expect(result).to.equal('delete');
+                        expect(actionContext.getServiceMeta()).to.be.empty;
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    describe('getMiddleware', function () {
+        it('should return the fetchr middleware', function () {
+            expect(pluginInstance.getMiddleware()).to.be.an('function');
+        });
+    });
+
+    describe('dehydrate', function () {
+        it('should dehydrate its state correctly', function () {
+            expect(pluginInstance.dehydrate()).to.deep.equal({
+                xhrPath: 'custom/api'
+            });
+        });
+    });
+
+    describe('rehydrate', function () {
+        it('should rehydrate the state correctly', function () {
+            pluginInstance.rehydrate({
+                xhrPath: 'custom2/api'
+            });
+            expect(pluginInstance.dehydrate()).to.deep.equal({
+                xhrPath: 'custom2/api'
+            });
+        });
+    });
+
+});
